587bbfdc99988ff4596d9b445f3ad34e0413bcf3
malinaq3 <malinaq3@gmail.com>
2020-05-18 00:03:38 +0200

    Both fibonacci works

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,31 @@
 #pragma once
+#include<iostream>
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+
+
+int tab[100];
+if (sequence < 0) {
+        std::cout << "Liczba nie moze byc ujemna!";
+
+} else if (sequence <= 1) {
+        return(sequence);
+}
+for (size_t i=2; i <= sequence; i++){
+    tab[0] = 0;
+    tab[1] = 1;
+    tab[i] = tab[i-1] + tab[i-2];
+}
+    return (tab[sequence]);
+    
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+
+    if (sequence < 0) {
+        std::cout << "Liczba nie moze byc ujemna!";
+    } else if (sequence <= 1) {
+        return(sequence);
+    }
+    return (fibonacci_recursive(sequence - 1) + (fibonacci_recursive(sequence - 2)));
 }
