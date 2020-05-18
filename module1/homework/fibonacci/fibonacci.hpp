af1c0bf71f9773eec084aab0d4b38526c937b16f
malinaq3 <malinaq3@gmail.com>
2020-05-18 21:07:20 +0200

    Changed to integers and loop only

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -4,19 +4,22 @@
 int fibonacci_iterative(int sequence) {
 
 
-int tab[100];
+
 if (sequence < 0) {
         std::cout << "Liczba nie moze byc ujemna!";
 
 } else if (sequence <= 1) {
         return(sequence);
 }
+    int num1 = 0, num2 = 1, temp;
+
 for (size_t i=2; i <= sequence; i++){
-    tab[0] = 0;
-    tab[1] = 1;
-    tab[i] = tab[i-1] + tab[i-2];
+
+    temp = num2;
+    num2 += num1;
+    num1 = temp;
 }
-    return (tab[sequence]);
+    return (num2);
     
 }
 
