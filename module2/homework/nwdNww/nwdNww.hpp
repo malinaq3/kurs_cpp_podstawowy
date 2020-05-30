e250bb93ea108bd74a2fd05785ad0dee4f554b19
malinaq3 <malinaq3@gmail.com>
2020-05-30 19:03:32 +0200

    NWD implemented

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,8 +1,13 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    int num;
+    while (rhs != 0) {
+        num = rhs;
+        rhs = lhs % rhs;
+        lhs = num;
+    }
+    return abs(lhs);
 }
 
 int NWW(int lhs, int rhs) {
