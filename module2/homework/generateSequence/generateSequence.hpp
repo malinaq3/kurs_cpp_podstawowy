6af10b06924200d820c80a5f547883fc68a591b4
Adrian Baranowicz <adrbaranowicz@gmail.com>
2020-05-31 21:13:59 +0200

    generateSequence implemented

--- module2/homework/generateSequence/generateSequence.hpp
+++ module2/homework/generateSequence/generateSequence.hpp
@@ -2,6 +2,17 @@
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+    if (count <= 0) {
+        return {};
+    }
+    else {
+        std::vector<int> sequence;
+        sequence.reserve(count);
+
+        for (size_t i = 1; i <= count; ++i) {
+            sequence.push_back(step * i);
+        }
+
+        return sequence;
+    }
 }
