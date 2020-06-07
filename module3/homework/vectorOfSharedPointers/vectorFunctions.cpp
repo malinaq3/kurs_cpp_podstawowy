88bd9926b7ff0d16c2d4736d0b2262edbcb15b1d
malinaq3 <malinaq3@gmail.com>
2020-06-08 00:18:53 +0200

    vectorFunctions suggestions added

--- module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
@@ -4,10 +4,8 @@
 
 std::vector<std::shared_ptr<int>> generate(int count) {
     std::vector<std::shared_ptr<int>> vec;
-    vec.reserve(count);
-    for (size_t i = 0; i < count; i++) {
+    for (int i = 0; i < count; i++) {
         vec.emplace_back(std::make_shared<int>(i));
-        auto size = vec.size();
         }
         return vec;
 }
@@ -19,7 +17,7 @@ void print (const std::vector<std::shared_ptr<int>>& vec) {
     std::cout << '\n';
 }
 
-void add10(std::vector<std::shared_ptr<int>>& vec) {
+void add10(const std::vector<std::shared_ptr<int>>& vec) {
     for (auto & el :vec) {
         if(el){
             *el += 10;
@@ -28,11 +26,12 @@ void add10(std::vector<std::shared_ptr<int>>& vec) {
 }
 
 void sub10(int * const num) {
-    if(num) *num -= 10;
+    if(num) 
+        *num -= 10;
 }
 
-void sub10(std::vector<std::shared_ptr<int>>& vec) {
-    for (auto& el : vec) {
+void sub10(const std::vector<std::shared_ptr<int>>& vec) {
+    for (const auto& el : vec) {
         sub10(el.get());
     }
 }
--- module3/homework/vectorOfSharedPointers/vectorFunctions.hpp
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.hpp
@@ -5,6 +5,6 @@
 
 std::vector<std::shared_ptr<int>> generate(int count);
 void print (const std::vector<std::shared_ptr<int>>& vec);
-void add10(std::vector<std::shared_ptr<int>>& vec);
+void add10(const std::vector<std::shared_ptr<int>>& vec);
 void sub10(int * const num);
-void sub10(std::vector<std::shared_ptr<int>>& vec);
+void sub10(const std::vector<std::shared_ptr<int>>& vec);
